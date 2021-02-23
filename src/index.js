add4a8969c393f02c59f58305a144c987391329b
JJ <jj.vega86@gmail.com>
2021-02-23 08:19:08 -0500

    Added state to App component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { latitude: "" };
+  }
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
