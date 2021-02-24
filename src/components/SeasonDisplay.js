ecdfe5c34b92e796c92f5941670fc9f332f69aa0
JJ <jj.vega86@gmail.com>
2021-02-24 08:45:43 -0500

    Refactored SeasonDisplay to use a config object for winter/summer options, implemented using object destructuring in return

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+const seasonConfig = { // design pattern to create this config object for each component you create. Simplifies code
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Burr, it's chilly",
+        iconName: 'snowflake'
+    }
+}
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     // if we're in summer months, return summer. Otherwise, return winter
@@ -11,14 +21,12 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text =
-    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach!";
-  const icon = season === "winter" ? "snowflake" : "sun";
+  const {text, iconName} = seasonConfig[season];
   return (
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
