5e6de9c321aa257f977824a617fe1de8749aa6e8
JJ <jj.vega86@gmail.com>
2021-02-24 08:17:08 -0500

    Added getSeason function to SeasonDisplay component

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,6 +1,17 @@
 import React from "react";
 
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9){ // if we're in summer months, return summer. Otherwise, return winter
+        return lat > 0 ? 'summer' : 'winter'; // northern or southern hemisphere
+    }else{
+        return lat > 0 ? 'winter' : 'summer';
+    }
+
+}
+
 const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth())
+    console.log(season);
   return <div>{props.lat}</div>;
 };
 
